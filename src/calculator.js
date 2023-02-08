d94aab2c9416576ed8a83c83286b2fba5f2733d4
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-08 11:01:35 -0800

    created method structure for lon

--- src/calculator.js
+++ src/calculator.js
@@ -8,8 +8,16 @@ class Calculator {
     }
 
     // TODO: make a water method
+    waterCalculator(numOfPeople = 0, waterByLiter) {
+        // 3.7 L per person per day
+
+    }
 
     // TODO: make a food method
+    foodCalculator(numOfPeople = 0, foodByLiter) {
+        // 2,000 calories per day per person
+
+    }
 }
 
 export default Calculator
