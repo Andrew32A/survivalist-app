9e1aeccd108fd146ff5e751d704ef8f26bdd383c
jcduff00 <johncduffman@gmail.com>
2023-02-08 14:31:32 -0500

    wrote food calculator logic

--- src/calculator.js
+++ src/calculator.js
@@ -8,7 +8,7 @@ class Calculator {
     }
 
     // TODO: make a water method
-    waterCalculator(days, numOfPeople = 0) {
+    waterCalculator(numOfPeople = 0, waterByLiter) {
         // 3.7 L per person per day
         let outputDays = 3.7 * numOfPeople
         let result = outputDays * days
@@ -19,11 +19,19 @@ class Calculator {
     // TODO: make a food method
     foodCalculator(days, foodByCalorie, numOfPeople = 0) {
         // 2,000 calories per day per person
-
+        let foodByCalorie = 2000
+        let outputDays = foodByCalorie * numOfPeople
+        let result = outputDays * days 
+        console.log(result)
+        return result
     }
 }
 
 const newCaluculator = new Calculator("demo")
 newCaluculator.waterCalculator(100, 2)
+newCaluculator.foodCalculator(1, 1, 1)
 
 export default Calculator
+
+console.log("change")
+console.log("hello")
