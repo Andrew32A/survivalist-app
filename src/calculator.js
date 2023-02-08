25067f28d2c96cb991b6b5b4c78c7783b212ed6c
jcduff00 <johncduffman@gmail.com>
2023-02-08 14:39:21 -0500

    added contextual comments, added arguments for water/food calc, removed redundant variable

--- src/calculator.js
+++ src/calculator.js
@@ -8,7 +8,8 @@ class Calculator {
     }
 
     // TODO: make a water method
-    waterCalculator(numOfPeople = 0, waterByLiter) {
+    //3.7L/person/day based on federal regulations and emergency preparedness recommendations.
+    waterCalculator(days, numOfPeople = 0) {
         // 3.7 L per person per day
         let outputDays = 3.7 * numOfPeople
         let result = outputDays * days
@@ -17,10 +18,11 @@ class Calculator {
     }
 
     // TODO: make a food method
-    foodCalculator(days, foodByCalorie, numOfPeople = 0) {
+    //2,000kcal/person/day based on standard health consultation for recommended daily consumption for average adults.
+    //Child logic to be added later.
+    foodCalculator(days, numOfPeople = 0) {
         // 2,000 calories per day per person
-        let foodByCalorie = 2000
-        let outputDays = foodByCalorie * numOfPeople
+        let outputDays = 2000 * numOfPeople
         let result = outputDays * days 
         console.log(result)
         return result
@@ -29,7 +31,7 @@ class Calculator {
 
 const newCaluculator = new Calculator("demo")
 newCaluculator.waterCalculator(100, 2)
-newCaluculator.foodCalculator(1, 1, 1)
+newCaluculator.foodCalculator(1, 1)
 
 export default Calculator
 
