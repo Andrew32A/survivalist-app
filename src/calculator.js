01017cd3beb29e371f0ded9a2d43cc23ef0407b0
jcduff00 <johncduffman@gmail.com>
2023-02-15 15:07:33 -0500

    added variables for children and pets, added logic to calc

--- src/calculator.js
+++ src/calculator.js
@@ -9,9 +9,9 @@ class Calculator {
 
     // TODO: make a water method
     //3.7L/person/day based on federal regulations and emergency preparedness recommendations.
-    waterCalculator(days, numOfPeople = 0) {
+    waterCalculator(days, numAdults = 0, numChildren = 0, numPets = 0) {
         // 3.7 L per person per day
-        let outputDays = 3.7 * numOfPeople
+        let outputDays = (3.7 * numAdults) + (2.0 * numChildren) + (1.3 * numPets)
         let result = outputDays * days
         console.log(result)
         return result
@@ -20,9 +20,9 @@ class Calculator {
     // TODO: make a food method
     //2,000kcal/person/day based on standard health consultation for recommended daily consumption for average adults.
     //Child logic to be added later.
-    foodCalculator(days, numOfPeople = 0) {
+    foodCalculator(days, numAdults = 0, numChildren = 0, numPets = 0) {
         // 2,000 calories per day per person
-        let outputDays = 2000 * numOfPeople
+        let outputDays = (2000 * numAdults) + (1400 * numChildren) + (400 * numPets)
         let result = outputDays * days 
         console.log(result)
         return result
@@ -30,7 +30,7 @@ class Calculator {
 }
 
 const newCaluculator = new Calculator("demo")
-newCaluculator.waterCalculator(14, 2)
+newCaluculator.waterCalculator(100, 2)
 newCaluculator.foodCalculator(1, 1)
 
 export default Calculator
