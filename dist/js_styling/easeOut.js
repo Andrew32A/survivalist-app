44c0037bc57c819bb7633652dae3a81749b5396e
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-23 14:30:51 -0800

    added animation library, animated info, linked pictures, and reorganized file structure

---
+++ dist/js_styling/easeOut.js
@@ -0,0 +1,26 @@
+anime({
+	targets: '.natural-disaster-wrapper',
+	opacity: 2,
+	duration: 4800,
+	translateY: ["-30px", "0px"],
+	easing: "easeOutExpo",
+	delay: (el, i) => 200 * i,
+})
+
+anime({
+	targets: '.economic-disaster-wrapper',
+	opacity: 2,
+	duration: 4800,
+	translateY: ["-30px", "0px"],
+	easing: "easeOutExpo",
+	delay: (el, i) => 200 * i,
+})
+
+anime({
+	targets: '.zombie-apocalypse-wrapper',
+	opacity: 2,
+	duration: 4800,
+	translateY: ["-30px", "0px"],
+	easing: "easeOutExpo",
+	delay: (el, i) => 200 * i,
+})
